41cdecfbc9395a2c7ac0169da435fb54c3c65fef
Rafael Nuñez <rafav.t.nunez@gmail.com>
2023-10-31 10:19:38 -0300

    Ajustes Mapas

--- Cientistas-Brilhantes/Assets/Scripts/TeleportToScene.cs
+++ Cientistas-Brilhantes/Assets/Scripts/TeleportToScene.cs
@@ -3,24 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TeleportToScene : MonoBehaviour
+public class ShowImageOnCollision : MonoBehaviour
 {
     [SerializeField]
-    private string sceneToLoad;  // The name of the scene to load.
+    private GameObject imageToShow; // Referência ao objeto da imagem que você deseja mostrar.
+    [SerializeField]
+    private string sceneToLoad;    // Nome da cena para carregar (se necessário).
 
     private void OnCollisionEnter(Collision collision)
     {
-        // Check if the player collided with an object tagged as "LoadSceneTrigger"
+        // Verifica se o jogador colidiu com um objeto marcado como "LoadSceneTrigger"
         if (collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("Player collided with " + gameObject.name);
-            LoadScene();
+
+            // Verifica se há um objeto de imagem para mostrar.
+            if (imageToShow != null)
+            {
+                imageToShow.SetActive(true); // Ativa o objeto da imagem.
+            }
+
+            // Se necessário, carrega a cena especificada.
+            if (!string.IsNullOrEmpty(sceneToLoad))
+            {
+                LoadScene();
+            }
         }
         else
         {
             Debug.Log("Player collided with " + gameObject.name + " but it is not tagged as Player");
         }
-
     }
 
     private void LoadScene()
